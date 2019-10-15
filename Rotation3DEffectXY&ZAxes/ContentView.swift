1cfffd236f1e5eb7797d49a3d40132596bfcf732
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-15 09:13:06 +0300

    init

--- Rotation3DEffectXY&ZAxes/ContentView.swift
+++ Rotation3DEffectXY&ZAxes/ContentView.swift
@@ -9,8 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var degrees = -65.0
     var body: some View {
-        Text("Hello World")
+        VStack(spacing: 10) {
+            Text("Rotation 3D Effect").font(.largeTitle)
+            Text("X, Y & Z Axes").font(.title).foregroundColor(.gray)
+            Text("Now it gets a little more complicated to think on all 3 axes. Plot the vector using X, Y and Z values.")
+            
+            Text("axis: (x: 2.0, y: 4.0, z: 3.0)")
+            Image("XYZ")
+            
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.yellow)
+                .opacity(0.7)
+                .overlay(
+                    Text("Move slider to adjust rotation")
+                        .font(.largeTitle).bold()
+            )
+                .rotation3DEffect(Angle(degrees: degrees), axis: (x: 2.0, y: 4.0, z: 3.0))
+            
+            Slider(value: $degrees, in: -180...180, step: 1).padding()
+            
+            Text("Crazy!")
+        }
     }
 }
 
